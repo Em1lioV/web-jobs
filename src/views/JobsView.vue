bbba01e95f4e0e68f42fcf255ce610fb5a498716
Em1lioV <emilio.villagrawt@gmail.com>
2023-09-11 20:11:18 -0300

    eliminando cosas hello word - agregando nuevo jobs view

---
+++ src/views/JobsView.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <h1>jobs</h1>
+        <div v-for="job in jobs" :key="job.id">
+            <h2>{{ job.title }}</h2>
+            <p>{{ job.detail }}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'JobsView',
+    data() {
+        return {
+            jobs:[
+                {title: 'ux designer', id:1, detail:'lorem'},
+                {title: 'frontend developer', id:2, detail:'lorem'},
+                {title: 'backend developer', id:2, detail:'lorem'}
+            ]
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
